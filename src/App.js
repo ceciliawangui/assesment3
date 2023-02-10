fc4698ad9750ca71e5ecc6738a96ffaba3450c59
git.assignment-1 <cesskui4034@gmail.com>
2023-02-10 11:09:27 +0300

    assesment

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from 'react';
+import './App.css';
+
+class App extends React.components { 
+  render(){ 
+    var user={ 
+   name: "John Doe",
+    Id:1,
+    career:'software engneer'
+    }}
+  }
+  <div>
+this.render(){ 
+ <h1>Name:"John Doe", Id:1, career:"software engneer"</h1>,
+    <h2>Name:"Jane Doe", Id:2, career:"data scientist"</h2>,
+    <h3>Name:"Billy Doe", Id:3, career:"cyber security analyst"</h3>,
+}
+  </div>
+
+  
